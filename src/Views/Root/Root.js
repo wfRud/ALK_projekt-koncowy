ccd00a575b53bef2ac5ee7214832d4e86d7a659b
Filip Rudek <rudek.filip@gmail.com>
2021-06-07 16:17:37 +0200

    Relocated App.js file as Root.js view in View directory

---
+++ src/Views/Root/Root.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Root.module.scss";
+import Mem from "../../components/Mem/Mem";
+
+function Root() {
+  return (
+    <div className="App">
+      <Mem />
+    </div>
+  );
+}
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./assets/style/global.scss";
+
+import Root from "./Views/Root/Root";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById("root")
 );
