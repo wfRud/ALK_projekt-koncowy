3d69f703f1e01f1788c7de2f49e031a232499716
Filip Rudek <rudek.filip@gmail.com>
2021-06-23 09:55:45 +0200

    Added burger btn

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,44 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navigation.module.scss";
 import { NavLink } from "react-router-dom";
 
-const Navigation = () => (
-  <nav className={styles.navigation}>
-    <span className={styles.logo_cnt}>
-      <h2 className={styles.logo}>MEME</h2>
-    </span>
+const Navigation = () => {
+  const [active, setActive] = useState(false);
 
-    <ul className={styles.nav_items}>
-      <NavLink
-        to="/regular"
-        className={`${styles.nav_item} meme_button`}
-        activeClassName={styles.active}
-      >
-        Regular
-      </NavLink>
-      <NavLink
-        to="/hot"
-        className={`${styles.nav_item} meme_button`}
-        activeClassName={styles.active}
-      >
-        Hot
-      </NavLink>
-      <NavLink
-        to="/favorite"
-        className={`${styles.nav_item} meme_button`}
-        activeClassName={styles.active}
-      >
-        Favorite
-      </NavLink>
-      <NavLink
-        to="/add"
-        className={`${styles.nav_item} meme_button`}
-        activeClassName={styles.active}
-      >
-        New
-      </NavLink>
-    </ul>
-  </nav>
-);
+  const handleBurgerAction = () => setActive(!active);
+  return (
+    <div className={styles.container}>
+      <nav className={styles.navigation}>
+        <span className={styles.logo_cnt}>
+          <h2 className={styles.logo}>MEME</h2>
+        </span>
+        <ul className={styles.nav_items}>
+          <NavLink
+            to="/regular"
+            className={`${styles.nav_item} meme_button`}
+            activeClassName={styles.active}
+          >
+            Regular
+          </NavLink>
+          <NavLink
+            to="/hot"
+            className={`${styles.nav_item} meme_button`}
+            activeClassName={styles.active}
+          >
+            Hot
+          </NavLink>
+          <NavLink
+            to="/favorite"
+            className={`${styles.nav_item} meme_button`}
+            activeClassName={styles.active}
+          >
+            Favorite
+          </NavLink>
+          <NavLink
+            to="/add"
+            className={`${styles.nav_item} meme_button`}
+            activeClassName={styles.active}
+          >
+            New
+          </NavLink>
+        </ul>
+      </nav>
+
+      <div className={styles.burger_container}>
+        <div
+          className={`${styles.burger} ${active ? styles.burger__active : ""}`}
+          onClick={handleBurgerAction}
+        >
+          <span
+            className={`${styles.burger_line} ${styles.burger_line_1}`}
+          ></span>
+          <span
+            className={`${styles.burger_line} ${styles.burger_line_2}`}
+          ></span>
+          <span
+            className={`${styles.burger_line} ${styles.burger_line_3}`}
+          ></span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navigation;
