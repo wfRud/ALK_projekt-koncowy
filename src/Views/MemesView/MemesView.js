cf5f77d39b1c980faeec2448aadd5783c593a110
Filip Rudek <rudek.filip@gmail.com>
2021-06-23 09:42:33 +0200

    Added Flex container for MemesView

--- src/Views/MemesView/MemesView.js
+++ src/Views/MemesView/MemesView.js
@@ -5,42 +5,32 @@ import travoltaGif from "../../assets/images/travolta_confused.gif";
 
 const MemesView = ({ list, handleVote, handleSetFave }) => (
   <div className={styles.container}>
-    <div className={styles.memesContainer}>
-      {list.length > 0 ? (
-        list.map((listItem) => {
-          const {
-            id,
-            title,
-            upvote,
-            downvote,
-            favorite,
-            img,
-            source,
-          } = listItem;
+    {list.length > 0 ? (
+      list.map((listItem) => {
+        const { id, title, upvote, downvote, favorite, img, source } = listItem;
 
-          return (
-            <Mem
-              id={id}
-              key={id}
-              title={title}
-              upvote={upvote}
-              downvote={downvote}
-              favorite={favorite}
-              img={img}
-              source={source}
-              handleVote={handleVote}
-              handleSetFave={handleSetFave}
-            />
-          );
-        })
-      ) : (
-        <img
-          src={travoltaGif}
-          alt="travolta gif"
-          className={styles.arrayEmptyGif}
-        />
-      )}
-    </div>
+        return (
+          <Mem
+            id={id}
+            key={id}
+            title={title}
+            upvote={upvote}
+            downvote={downvote}
+            favorite={favorite}
+            img={img}
+            source={source}
+            handleVote={handleVote}
+            handleSetFave={handleSetFave}
+          />
+        );
+      })
+    ) : (
+      <img
+        src={travoltaGif}
+        alt="travolta gif"
+        className={styles.arrayEmptyGif}
+      />
+    )}
   </div>
 );
 
