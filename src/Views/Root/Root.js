89a8d90160f396dc2d0cdb28b6af3fac31a17ae9
Filip Rudek <rudek.filip@gmail.com>
2021-06-26 17:43:50 +0200

    Deleted Route component parameter, to prevent top scroll page after render

--- src/Views/Root/Root.js
+++ src/Views/Root/Root.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import styles from "./Root.module.scss";
@@ -9,11 +9,16 @@ import FormView from "../FormView/FormView";
 import * as listActions from "../../store/list/actions";
 
 function Root() {
-  const state = useSelector((state) => state);
-  const list = useSelector((state) => state.mainList);
-  const regularList = useSelector((state) => state.regularList);
-  const hotList = useSelector((state) => state.hotList);
-  const favoriteList = useSelector((state) => state.favoriteList);
+  const mainList = useSelector((state) => state.mainList);
+  // const { mainList: list } = state;
+
+  const [regularList, setRegularList] = useState([]);
+  const [hotList, setHotList] = useState([]);
+  const [favoriteList, setFavoriteList] = useState([]);
+
+  // const regularList = useSelector((state) => state.regularList);
+  // const hotList = useSelector((state) => state.hotList);
+  // const favoriteList = useSelector((state) => state.favoriteList);
 
   const dispatch = useDispatch();
 
@@ -29,80 +34,78 @@ function Root() {
     dispatch(listActions.setFave(currentId));
   };
 
-  const clearSpecificArray = () => {
-    for (const key in state) {
-      if (
-        Array.isArray(state[key]) &&
-        key !== "mainList" &&
-        state[key].length > 0
-      ) {
-        dispatch(listActions.clear(key));
-      }
-    }
-  };
+  // const clearSpecificArray = () => {
+  //   for (const key in state) {
+  //     if (
+  //       Array.isArray(state[key]) &&
+  //       key !== "mainList" &&
+  //       state[key].length > 0
+  //     ) {
+  //       dispatch(listActions.clear(key));
+  //     }
+  //   }
+  // };
 
   const setArrays = (arr) => {
-    let isHot = false;
-    clearSpecificArray();
+    const regularArr = arr.filter((meme) => meme.upvote - meme.downvote <= 5);
+    const hotArr = arr.filter((meme) => meme.upvote - meme.downvote > 5);
+    const favoriteArr = arr.filter((meme) => meme.favorite);
 
-    arr.forEach((meme) => {
-      meme.upvote - meme.downvote > 5 ? (isHot = true) : (isHot = false);
-      dispatch(listActions.insert(isHot, meme));
-      return meme.favorite ? dispatch(listActions.insertFave(meme)) : null;
-    });
+    setRegularList([...regularArr]);
+    setHotList([...hotArr]);
+    setFavoriteList([...favoriteArr]);
+
+    // arr.forEach((meme) => {
+    //   meme.upvote - meme.downvote > 5
+    //     ? dispatch(listActions.insert(true, meme))
+    //     : dispatch(listActions.insert(false, meme));
+
+    //   return meme.favorite ? dispatch(listActions.insertFave(meme)) : null;
+    // });
   };
 
   useEffect(() => {
-    setArrays(list);
-    list.length > 0 &&
-      window.localStorage.setItem("mainList", JSON.stringify(list));
-  }, [list]);
+    window.localStorage.getItem("mainList") &&
+      dispatch(
+        listActions.setMainList(
+          JSON.parse(window.localStorage.getItem("mainList"))
+        )
+      );
+  }, []);
 
   useEffect(() => {
-    window.localStorage.getItem("mainList")
-      ? dispatch(
-          listActions.setMainList(
-            JSON.parse(window.localStorage.getItem("mainList"))
-          )
-        )
-      : dispatch(listActions.setMainList([]));
-  }, []);
+    setArrays(mainList);
+    mainList.length > 0 &&
+      window.localStorage.setItem("mainList", JSON.stringify(mainList));
+  }, [mainList]);
 
   return (
     <div className={styles.App}>
       <Router>
         <Navigation />
         <Route path="/" component={FormView} exact />
-        <Route
-          path="/regular"
-          component={() => (
-            <MemesView
-              list={regularList}
-              handleVote={handleVote}
-              handleSetFave={handleSetFave}
-            />
-          )}
-        />
-        <Route
-          path="/hot"
-          component={() => (
-            <MemesView
-              list={hotList}
-              handleVote={handleVote}
-              handleSetFave={handleSetFave}
-            />
-          )}
-        />
-        <Route
-          path="/favorite"
-          component={() => (
-            <MemesView
-              list={favoriteList}
-              handleVote={handleVote}
-              handleSetFave={handleSetFave}
-            />
-          )}
-        />
+        <Route path="/regular">
+          <MemesView
+            list={regularList}
+            handleVote={handleVote}
+            handleSetFave={handleSetFave}
+          />
+        </Route>
+
+        <Route path="/hot">
+          <MemesView
+            list={hotList}
+            handleVote={handleVote}
+            handleSetFave={handleSetFave}
+          />
+        </Route>
+        <Route path="/favorite">
+          <MemesView
+            list={favoriteList}
+            handleVote={handleVote}
+            handleSetFave={handleSetFave}
+          />
+        </Route>
       </Router>
     </div>
   );
