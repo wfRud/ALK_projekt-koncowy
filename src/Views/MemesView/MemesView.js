b743a5d909c8a9df849c3a4ee5b07e8dd9566834
Filip Rudek <rudek.filip@gmail.com>
2021-06-26 17:37:56 +0200

    delete curly braces in return component

--- src/Views/MemesView/MemesView.js
+++ src/Views/MemesView/MemesView.js
@@ -5,16 +5,14 @@ import Mem from "../../components/Mem/Mem";
 const MemesView = ({ list, handleVote, handleSetFave }) => (
   <div className={styles.container}>
     {list.length > 0 ? (
-      list.map((listItem) => {
-        return (
-          <Mem
-            key={listItem.id}
-            {...listItem}
-            handleVote={handleVote}
-            handleSetFave={handleSetFave}
-          />
-        );
-      })
+      list.map((listItem) => (
+        <Mem
+          key={listItem.id}
+          {...listItem}
+          handleVote={handleVote}
+          handleSetFave={handleSetFave}
+        />
+      ))
     ) : (
       <div className={styles.message}>There is no content to display</div>
     )}
